07e1d7f0808b5130fe7dc6c61d421dccf6e77d4c
wickwickyiii <wickham.eaw@gmail.com>
2018-08-21 16:27:30 -0500

    8/21 updates to index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,9 @@ const port = 3000;
 const app = express();
 
 app.use(bodyParser.json());
+app.use(express.static(_dirname + '/../public/build/'));
 
+const messageBaseURL = "/api/messages";
 app.get("/api/messages", mc.read);
 app.put("/api/messages/:id", mc.update);
 app.post("/api/messages", mc.create);
